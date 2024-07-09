53fe917343c3cb89ceecc7711554451160f8f122
Jeremy Crane <jeremyccrane@me.com>
2024-07-08 23:09:54 -0600

    Update entrypoint_node99.sh

--- entrypoint_node99.sh
+++ entrypoint_node99.sh
@@ -66,7 +66,7 @@ sleep 10
 # Start Geth and enable mining
 echo "Starting Geth on node99 and enabling mining"
 exec geth --datadir /data \
-    --syncmode "fullarchived" \
+    --syncmode "full" \
     --http \
     --http.addr 0.0.0.0 \
     --http.port 8546 \
